fd1b1526fa20a7e7e081aada48ca7401b2503e86
LordAndy316 <LordAndy316@gmail.com>
2024-06-18 15:15:28 -0400

    Added lander falling and steering
    Directional Lander control
    gravity increases after timer is depleted
    debug hud to show timer

--- player.gd
+++ player.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
 
-const SPEED = 50
-const MAX_THRUST = -50
+
+const MAX_THRUST = -10
 @onready var player = $"."
-var thrust = -5
+var thrust = -1
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -12,31 +12,34 @@ var fallingtimer
 func exp_decay(a,b,decay,dt):
 	return (b+(a-b)*exp(-decay*dt))
 
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor() and fallingtimer:
-		velocity.y += gravity * delta
-	
-	# Handle jump.
-	if Input.is_action_pressed("thrust"):
-		thrust += thrust * (1-thrust/MAX_THRUST)
-		thrust = exp_decay(thrust, MAX_THRUST ,1, delta)
-		print(thrust)
-		velocity.y = thrust
-	exp_decay(thrust,0,1,delta)
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("left", "right")
-	if direction:
-		player.rotate(-0.1)
-	else:
-		player.rotate(0.1)
-
-	
-
-
 func _on_ready():
 	pass # Replace with function body.
 
+func _physics_process(delta):
+	# Add the gravity.
+	velocity.y += gravity * delta
+	if not is_on_floor() and fallingtimer:
+		gravity = 200
+	
+	# Handle thrust.
+	if Input.is_action_pressed("thrust"):
+		thrust += thrust * (1-thrust/MAX_THRUST)
+		thrust = exp_decay(thrust, MAX_THRUST ,1, delta)
+		print("thrust")
+	
+	velocity += Vector2(0, 0.5).rotated(rotation) * thrust
+	thrust = exp_decay(thrust,0,5,delta)
+	
+	
+	
+	# Get the input direction and handle the movement/deceleration.
+	var direction = Input.get_axis("left", "right")
+	if direction >0.9:
+		player.rotate(-0.05)
+	elif direction < -0.9 :
+		player.rotate(0.05)
+	move_and_slide()
+
 func _landing_mode():
 	fallingtimer = true
+	print("time to fall")
--- world.gd
+++ world.gd
@@ -1,15 +1,26 @@
 extends Node2D
 
-
+@onready var ground = $"Ground"
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
 	pass
-
+func _touchdown():
+	return ground
 
 func _on_timer_timeout():
 	$Player._landing_mode()
+
+
+
+func _on_ground_input_event(viewport, event, shape_idx):
+	print(viewport)
+	print(event)
+	print(shape_idx)
+	pass # Replace with function body.
